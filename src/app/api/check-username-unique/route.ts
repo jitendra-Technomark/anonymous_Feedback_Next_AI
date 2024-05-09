4018af13db9abec9473c2404a42ae1e651f57150
Jitendra Kumar <jitendra.kumar@technomark.io>
2024-05-09 12:48:05 +0530

    completed backend and API part

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -1,6 +1,6 @@
+import { z } from "zod";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { z } from "zod";
 import { usernameValidation } from "@/schemas/signUpSchema";
 
 const UsernameQuerySchema = z.object({
